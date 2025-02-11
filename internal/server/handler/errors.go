73deab96c17c2e4a9ae5237a9896fafbfa1f4cc4
0x0FACED <https.genius@gmail.com>
2025-02-11 20:04:23 +0300

    fix: changed errors mapping (using database errors, not service)

--- internal/server/handler/errors.go
+++ internal/server/handler/errors.go
@@ -4,32 +4,34 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/0x0FACED/merch-shop/internal/database"
 	"github.com/0x0FACED/merch-shop/internal/service"
 )
 
-// MapServiceErrorToStatusCode маппит ошибки, полученные из Базы данных -> сервиса в статус коды
+// MapServiceErrorToStatusCode маппит ошибки, полученные из базы в статус коды
 func MapServiceErrorToStatusCode(err error) int {
 	switch {
 	// 401 — Ошибки аутентификации
-	case errors.Is(err, service.ErrInvalidLoginOrPassword):
+	case errors.Is(err, database.ErrInvalidLoginOrPassword),
+		errors.Is(err, service.ErrFailedComparingHashAndPassword):
 		return http.StatusUnauthorized
 
 	// 400 — Ошибки, связанные с неверными входными данными
-	case errors.Is(err, service.ErrInsufficientFunds),
-		errors.Is(err, service.ErrFailedToFindRecipient),
-		errors.Is(err, service.ErrNotFound):
+	case errors.Is(err, database.ErrInsufficientFunds),
+		errors.Is(err, database.ErrFailedToFindRecipient),
+		errors.Is(err, database.ErrNotFound):
 		return http.StatusBadRequest
 
 	// 500 — Внутренние ошибки базы и транзакций
-	case errors.Is(err, service.ErrQueryFailed),
-		errors.Is(err, service.ErrScanFailed),
-		errors.Is(err, service.ErrRowsFailed),
-		errors.Is(err, service.ErrFailedToBeginTx),
-		errors.Is(err, service.ErrFailedToFetchBalance),
-		errors.Is(err, service.ErrFailedToDebitSender),
-		errors.Is(err, service.ErrFailedToCreditRecipient),
-		errors.Is(err, service.ErrFailedToSaveTransaction),
-		errors.Is(err, service.ErrFailedToCommitTx):
+	case errors.Is(err, database.ErrQueryFailed),
+		errors.Is(err, database.ErrScanFailed),
+		errors.Is(err, database.ErrRowsFailed),
+		errors.Is(err, database.ErrFailedToBeginTx),
+		errors.Is(err, database.ErrFailedToFetchBalance),
+		errors.Is(err, database.ErrFailedToDebitSender),
+		errors.Is(err, database.ErrFailedToCreditRecipient),
+		errors.Is(err, database.ErrFailedToSaveTransaction),
+		errors.Is(err, database.ErrFailedToCommitTx):
 		return http.StatusInternalServerError
 
 	// 500 по дефолту
