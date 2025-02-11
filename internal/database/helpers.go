6f4394c4980d9deaa5a46e03d299a03c80b918ba
0x0FACED <https.genius@gmail.com>
2025-02-11 14:06:07 +0300

    chore: added calc hash func using bcrypt with default cost

---
+++ internal/database/helpers.go
@@ -0,0 +1,11 @@
+package database
+
+import "golang.org/x/crypto/bcrypt"
+
+func calcHash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
