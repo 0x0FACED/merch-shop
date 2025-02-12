8f2c189f30acb8545a42a64428bb63738c5112b1
0x0FACED <https.genius@gmail.com>
2025-02-12 14:43:04 +0300

    refactor log level

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/0x0FACED/merch-shop/config"
+	"github.com/0x0FACED/merch-shop/internal/database"
 	"github.com/0x0FACED/merch-shop/internal/server"
+	"github.com/0x0FACED/merch-shop/internal/server/handler"
+	"github.com/0x0FACED/merch-shop/internal/service"
+	"github.com/0x0FACED/merch-shop/pkg/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -16,12 +20,34 @@ func main() {
 	defer stop()
 
 	cfg := config.MustLoad()
-	srv, err := server.NewServer(cfg)
+	log := logger.New(cfg.Logger)
+
+	db, err := database.New(cfg.Database, log)
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Fatal("Failed to create database", zap.Error(err))
+	}
+	db.MustConnect(ctx)
+	defer db.Close()
+
+	merchService := service.NewUserService(db, log)
+
+	h := handler.NewHandler(merchService, log, &cfg.Server)
+
+	server, err := server.NewServer(cfg, h)
+	if err != nil {
+		log.Fatal("Failed to create server", zap.Error(err))
 	}
 
-	if err := srv.Start(ctx); err != nil {
-		log.Fatalf("Server stopped with err: %v", err)
+	go func() {
+		if err := server.Start(ctx); err != nil {
+			log.Error("Server stopped with error", zap.Error(err))
+		}
+	}()
+
+	<-ctx.Done()
+	log.Info("Shutting down gracefully...")
+
+	if err := server.Shutdown(); err != nil {
+		log.Fatal("Failed to shutdown server", zap.Error(err))
 	}
 }
