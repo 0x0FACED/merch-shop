41e1360d17e64e00ea6cfd689f16a8b8c882bc8e
0x0FACED <https.genius@gmail.com>
2025-02-12 14:45:45 +0300

    refactor: added pprof http server to server struct, removed db ptr

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,40 +5,27 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/http/pprof"
 	"time"
 
 	"github.com/0x0FACED/merch-shop/config"
-	"github.com/0x0FACED/merch-shop/internal/database"
 	"github.com/0x0FACED/merch-shop/internal/server/handler"
 	"github.com/0x0FACED/merch-shop/internal/server/validator"
-	"github.com/0x0FACED/merch-shop/internal/service"
 	"github.com/0x0FACED/merch-shop/pkg/logger"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Server struct {
-	echo   *echo.Echo
-	config config.ServerConfig
-	logger *logger.ZapLogger
-	db     *database.Postgres
+	echo     *echo.Echo
+	config   config.ServerConfig
+	logger   *logger.ZapLogger
+	handler  *handler.Handler
+	pprofSrv *http.Server // сервер для профилирвоания
 }
 
-func NewServer(cfg *config.ServiceConfig) (*Server, error) {
+func NewServer(cfg *config.ServiceConfig, h *handler.Handler) (*Server, error) {
 	log := logger.New(cfg.Logger)
-
-	log.Info("Config is loaded")
-
-	db, err := database.New(cfg.Database, log)
-	if err != nil {
-		return nil, fmt.Errorf("error creating database instance: %w", err)
-	}
-	db.MustConnect(context.Background())
-
-	userService := service.NewUserService(db, log)
-
-	log.Info("UserService created")
 
 	e := echo.New()
 	e.Server = &http.Server{
@@ -50,64 +37,73 @@ func NewServer(cfg *config.ServiceConfig) (*Server, error) {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 
-	h := handler.NewHandler(userService, log, &cfg.Server)
 	h.SetupRoutes(e)
 
-	return &Server{
-		echo:   e,
-		config: cfg.Server,
-		logger: log,
-		db:     db,
-	}, nil
-}
+	// Сервер для pprof
+	pprofMux := http.NewServeMux()
+	pprofMux.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
+	pprofMux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+	pprofMux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+	pprofMux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
+	pprofMux.Handle("/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
 
-func (s *Server) Start(ctx context.Context) error {
-	s.logger.Info("Starting server...")
-
-	errChan := make(chan error, 1)
-	go func() {
-		addr := fmt.Sprintf("%s:%s", s.config.Host, s.config.Port)
-		err := s.echo.Start(addr)
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			errChan <- err
-		}
-		close(errChan)
-	}()
-
-	select {
-	case <-ctx.Done():
-		s.logger.Info("Interrupt received, shutting down...")
-		return s.Shutdown()
-	case err := <-errChan:
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			return fmt.Errorf("server stopped with err: %w", err)
-		}
-		return nil
+	pprofSrv := &http.Server{
+		Addr:    ":6060",
+		Handler: pprofMux,
 	}
+
+	return &Server{
+		echo:     e,
+		config:   cfg.Server,
+		logger:   log,
+		handler:  h,
+		pprofSrv: pprofSrv,
+	}, nil
 }
 
 func (s Server) Echo() *echo.Echo {
 	return s.echo
 }
 
-func (s Server) Database() *pgxpool.Pool {
-	return s.db.Pool()
+func (s *Server) Start(ctx context.Context) error {
+	s.logger.Info("Starting server...")
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		addr := fmt.Sprintf("%s:%s", s.config.Host, s.config.Port)
+		if err := s.echo.Start(addr); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errChan <- err
+		}
+		close(errChan)
+	}()
+
+	go func() {
+		s.logger.Info("Starting pprof server on :6060")
+		if err := s.pprofSrv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			s.logger.Error("Pprof server error", err)
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		s.logger.Info("Received shutdown signal...")
+		return nil
+	case err := <-errChan:
+		return err
+	}
 }
 
 func (s *Server) Shutdown() error {
-	s.logger.Info("Shutting down the server...")
-
+	s.logger.Info("Shutting down server...")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := s.echo.Shutdown(ctx); err != nil {
-		return fmt.Errorf("failed to shutdown server: %w", err)
+		return err
 	}
-
-	if err := s.db.Close(); err != nil {
-		return fmt.Errorf("failed to close db: %w", err)
+	if err := s.pprofSrv.Shutdown(ctx); err != nil {
+		return err
 	}
-
-	s.logger.Info("Server shutdown successfully")
 	return nil
 }
