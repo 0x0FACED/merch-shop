1c140cf437035df988292ae0bbdf7d89f7c6046a
0x0FACED <https.genius@gmail.com>
2025-02-12 13:08:28 +0300

    chore: added validate tags

--- internal/server/handler/requests.go
+++ internal/server/handler/requests.go
@@ -1,11 +1,11 @@
 package handler
 
 type AuthRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required,alphanum,max=255"`
+	Password string `json:"password" validate:"required,alphanum,min=8,max=128"`
 }
 
 type SendCoinRequest struct {
-	ToUser string `json:"toUser"`
-	Amount int    `json:"amount"`
+	ToUser string `json:"toUser" validate:"required,alphanum,max=255"`
+	Amount int    `json:"amount" validate:"required,gt=0"`
 }
